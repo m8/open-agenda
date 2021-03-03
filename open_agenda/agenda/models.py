764552e74be48f92e61368ff85e9635d23e1cfd6
m8 <umusasadik@gmail.com>
2021-03-03 22:45:31 +0300

    python template engine

--- open_agenda/agenda/models.py
+++ open_agenda/agenda/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Notes(models.Model):
-    notes = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    notes = models.CharField(max_length=5000)
+    pub_date = models.CharField(max_length=50)
 
     def __str__(self):
         return self.notes
--- open_agenda/agenda/views.py
+++ open_agenda/agenda/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Notes
 # Create your views here.
+from datetime import date
 
 def index(req):
-    return render(req,'agenda.html')
+    today = date.today().strftime("%Y-%m-%d")
+    todays_note = Notes.objects.filter(pub_date=today)
+    
+    print(todays_note[0])
+    
+    return render(req,'agenda.html', context={"notes":todays_note})
