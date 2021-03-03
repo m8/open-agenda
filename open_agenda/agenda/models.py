22f03bf3ecb91e4c9c5b3962a2656883550c33da
m8 <umusasadik@gmail.com>
2021-03-04 00:42:04 +0300

    class fixed

--- open_agenda/agenda/models.py
+++ open_agenda/agenda/models.py
@@ -4,6 +4,3 @@ from django.db import models
 class Notes(models.Model):
     notes = models.CharField(max_length=5000)
     pub_date = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.notes
