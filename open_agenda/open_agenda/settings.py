6ce87356141a96742d3b79718e76583eac7272d6
m8 <umusasadik@gmail.com>
2021-03-03 20:38:36 +0300

    template directory added

--- open_agenda/open_agenda/settings.py
+++ open_agenda/open_agenda/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'open_agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
