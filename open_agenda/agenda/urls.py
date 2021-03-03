dad68128ceba351900f16f16ba3db0532fe607bc
m8 <umusasadik@gmail.com>
2021-03-04 00:42:22 +0300

    update url added

--- open_agenda/agenda/urls.py
+++ open_agenda/agenda/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'agenda'
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path(r'update/',views.updateAgenda)
 ]
