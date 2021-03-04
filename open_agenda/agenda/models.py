5f6fa06a03dd91763b58fb7e39e78972720e9b37
m8 <umusasadik@gmail.com>
2021-03-04 15:13:17 +0300

    get specific date

--- open_agenda/agenda/models.py
+++ open_agenda/agenda/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Notes(models.Model):
     notes = models.CharField(max_length=5000)
     pub_date = models.CharField(max_length=50)
+
+    # def __init__(self, notes,pub_date):
+    #     self.notes = notes,
+    #     self.pub_date = pub_date
--- open_agenda/agenda/views.py
+++ open_agenda/agenda/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import HttpResponse
+
 # Create your views here.
 from datetime import date
 
 def index(req):
     today = date.today().strftime("%Y-%m-%d")
     todays_note = Notes.objects.filter(pub_date=today)
-    
-    print(todays_note[0])
-    
-    return render(req,'agenda.html', context={"notes":todays_note})
+    if(todays_note.count() == 0):
+        note = Notes()
+        note.pub_date = today
+        return render(req,'agenda.html', context={"notes":note})
+    else:
+        return render(req,'agenda.html', context={"notes":todays_note[0]})
+
+
+def updateAgenda(req):
+    date = req.POST.get('date','')
+    notes = req.POST.get('notes','')
+
+    Notes.objects.update_or_create(
+        pub_date = date,
+        defaults={'notes': notes, 'pub_date': date},
+    )
+
+    return HttpResponse(200)
+
+def getDate(req):
+    req_date = req.GET.get('date', '')
+    note = Notes.objects.filter(pub_date=req_date)
+
+    if(note.count() == 0):
+        note = Notes()
+        note.pub_date = req_date
+        return render(req,'agenda.html', context={"notes":note})
+    else:
+        return render(req,'agenda.html', context={"notes":note[0]})
