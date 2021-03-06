b458a9c9f59992f03f7d31372daaa388f575e619
m8 <umusasadik@gmail.com>
2021-03-06 03:04:41 +0300

    project section

--- open_agenda/agenda/models.py
+++ open_agenda/agenda/models.py
@@ -8,3 +8,8 @@ class Notes(models.Model):
     # def __init__(self, notes,pub_date):
     #     self.notes = notes,
     #     self.pub_date = pub_date
+
+class Project(models.Model):
+    notes = models.CharField(max_length=50000)
+    icon = models.CharField(max_length=50)
+    name =  models.CharField(max_length=30)
--- open_agenda/agenda/views.py
+++ open_agenda/agenda/views.py
@@ -1,28 +1,16 @@
-from django.shortcuts import render
-from .models import Notes
+from django.shortcuts import render,redirect
+from .models import Notes,Project
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 from datetime import date
 import datetime
 
 def index(req):
     today = date.today().strftime("%d-%m-%Y")
-    yesterday = date.today() + datetime.timedelta(days=-1)
-    yesterday = yesterday.strftime("%d-%m-%Y")
+    return redirect('/get?date='+today)
 
-    tomorrow = date.today() + datetime.timedelta(days=1)
-    tomorrow = tomorrow.strftime("%d-%m-%Y")
-
-    todays_note = Notes.objects.filter(pub_date=today)
-    if(todays_note.count() == 0):
-        note = Notes()
-        note.pub_date = today
-        return render(req,'agenda.html', context={"notes":note})
-    else:
-        return render(req,'agenda.html', context={"notes":todays_note[0],"dates":[tomorrow,yesterday]})
-
-
+# @ post /update
 def updateAgenda(req):
     date = req.POST.get('date','')
     notes = req.POST.get('notes','')
@@ -38,33 +26,62 @@ def updateAgenda(req):
 def GetDate(req):
 
     req_date = req.GET.get('date', '')
-    note = Notes.objects.filter(pub_date=req_date)
-
-    formatted_date = datetime.datetime.strptime(req_date, '%d-%m-%Y')
-
-    yesterday = formatted_date + datetime.timedelta(days=-1)
-    yesterday = yesterday.strftime("%d-%m-%Y")
-
-    tomorrow = formatted_date + datetime.timedelta(days=1)
-    tomorrow = tomorrow.strftime("%d-%m-%Y")
-
-
-    if(note.count() == 0):
-        note = Notes()
-        note.pub_date = req_date
-        return render(req,'agenda.html', context={"notes":note, "dates":[tomorrow,yesterday]})
-    else:
-        return render(req,'agenda.html', context={"notes":note[0], "dates":[tomorrow,yesterday]})
+    req_project = req.GET.get('project','')
+    print(len(req_date),len(req_project))
+    projects = Project.objects.all()
     
+    if(len(req_date)>0):
+        note = Notes.objects.filter(pub_date=req_date)
+
+        formatted_date = datetime.datetime.strptime(req_date, '%d-%m-%Y')
+
+        yesterday = formatted_date + datetime.timedelta(days=-1)
+        yesterday = yesterday.strftime("%d-%m-%Y")
+
+        tomorrow = formatted_date + datetime.timedelta(days=1)
+        tomorrow = tomorrow.strftime("%d-%m-%Y")
+
+
+        if(note.count() == 0):
+            note = Notes()
+            note.pub_date = req_date
+            return render(req,'agenda.html', context={"notes":note, "dates":[tomorrow,yesterday],"projects": projects})
+        else:
+            return render(req,'agenda.html', context={"notes":note[0], "dates":[tomorrow,yesterday],"projects": projects})
+    
+    elif(len(req_project)>0):
+        try:
+            project = Project.objects.get(id=req_project)
+            return render(req,'project.html', context={"notes":project,"projects": projects})
+        except ObjectDoesNotExist:        
+            return redirect('/')
+
 # @ get /weekly
 def Weekly(req):
+    projects = Project.objects.all()
+
     today = date.today().strftime("%d-%m-%Y")
     days_of_the_week = get_week(today)
     print(days_of_the_week)
-    return render(req,'weekly.html',context={"days":days_of_the_week})
+    return render(req,'weekly.html',context={"days":days_of_the_week,"projects": projects})
 
 def Calendar(req):
-    return render(req,'calendar.html')
+    projects = Project.objects.all()
+
+    return render(req,'calendar.html',context={"projects": projects})
+
+
+# @ post /update
+def AddProject(req):
+    title = req.POST.get('title','')
+    icon = req.POST.get('icon','')
+
+    Project.objects.update_or_create(
+        name = title,
+        defaults={'name': title, 'icon': icon, 'notes':''},
+    )
+
+    return redirect('/')
 
 
 
@@ -74,3 +91,5 @@ def get_week(dt):
     dates=[str((start_date + datetime.timedelta(days=i)).date().strftime("%d-%m-%Y")) for i in range(7)]
     day_names=[str((start_date + datetime.timedelta(days=i)).date().strftime("%A")) for i in range(7)]
     return(zip(dates,day_names))
+
+
